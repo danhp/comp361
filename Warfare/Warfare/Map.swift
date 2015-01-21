e713b79a02ee057b65e18e443d62f0af9df131cd
justindomingue <justin.domingue@hotmail.com>
2015-01-21 14:07:54 -0500

    Added basic implementation of Map and Tile

---
+++ Warfare/Warfare/Map.swift
@@ -0,0 +1,13 @@
+//
+//  Map.swift
+//  warfare
+//
+//  Created by Justin Domingue on 2015-01-21.
+//  Copyright (c) 2015 Justin Domingue. All rights reserved.
+//
+
+import UIKit
+
+class Map: SKNode {
+   
+}
---
+++ Warfare/Warfare/Tile.swift
@@ -0,0 +1,13 @@
+//
+//  Tile.swift
+//  warfare
+//
+//  Created by Justin Domingue on 2015-01-21.
+//  Copyright (c) 2015 Justin Domingue. All rights reserved.
+//
+
+import UIKit
+
+class Tile: SKShapeNode {
+   
+}
