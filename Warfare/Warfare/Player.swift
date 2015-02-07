a37e52c1f8743d51ecd67be4725c207d6c2b2dfe
justindomingue <justin.domingue@hotmail.com>
2015-02-07 15:25:46 -0500

    Added player and basic implementation

---
+++ Warfare/Warfare/Player.swift
@@ -0,0 +1,20 @@
+//
+//  Player.swift
+//  Warfare
+//
+//  Created by Justin Domingue on 2015-02-07.
+//  Copyright (c) 2015 Justin Domingue. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    var villages = [Village]()
+    
+    /// Removes village v from the list of villages 
+    /// and removes the unit or structures from the
+    /// region controlled by that village
+    func removeVillages(v: Village) {
+        for u in v.un
+    }
+}
