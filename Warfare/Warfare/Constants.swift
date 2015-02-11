cc61b53084602a1473b8390f6946a11cd50c69af
Daniel Pham <pham.dany@gmail.com>
2015-02-10 21:37:54 -0500

    set correct wages for upkeep

--- Warfare/Warfare/Constants.swift
+++ Warfare/Warfare/Constants.swift
@@ -9,19 +9,19 @@
 import Darwin
 
 struct Constants {
-	
-	struct Map {
-		static let dimension = 18	// 18x18 maps
-	}
-	
-	struct Tile {
-		static let size = 40	// Distance from center to a corner
-	}
-	
-	struct Types {
-		 enum Land {
-			case Grass, Tree, Meadow, Sea
-            
+
+    struct Map {
+        static let dimension = 18	// 18x18 maps
+    }
+
+    struct Tile {
+        static let size = 40	// Distance from center to a corner
+    }
+
+    struct Types {
+         enum Land {
+            case Grass, Tree, Meadow, Sea
+
             func gold() -> Int {
                 switch self {
                 case .Meadow:
@@ -32,7 +32,7 @@ struct Constants {
                     return 1
                 }
             }
-            
+
             static func random() -> Land {
                 switch arc4random_uniform(14) {
                 case 0...2:
@@ -45,42 +45,44 @@ struct Constants {
                     return .Grass
                 }
             }
-		}
-		
-		enum Village: Int {
-			case Hovel = 1, Town, Fort
-		}
-		
-		enum Unit: Int {
-			case Peasant = 1, Infantry, Soldier, Knight
-            
+        }
+
+        enum Village: Int {
+            case Hovel = 1, Town, Fort
+        }
+
+        enum Unit: Int {
+            case Peasant = 1, Infantry, Soldier, Knight
+
             func wage() -> Int {
-                return self.rawValue
+                switch self {
+                case .Peasant:
+                    return 2
+                case .Infantry:
+                    return 6
+                case .Soldier:
+                    return 18
+                case .Knight:
+                    return 54
+                }
             }
-		}
-		
-		enum Structure {
-			case Tower, Road, Tombstone
-		}
+        }
+
+        enum Structure {
+            case Tower, Road, Tombstone
+        }
     }
-    
+
     struct Unit {
         enum Action {
             case ReadyForOrders, Moved, BuildingRoad, ChoppingTree, ClearingTombstone, UpgradingCombining, StartCultivating, FinishCultivating
         }
     }
 
-	struct Cost {
-		enum Upgrade: Int {
-			case Village = 8
-			case Unit = 10
-		}
-
-		enum Upkeep: Int {
-			case Peasant = 2
-			case Infantry = 6
-			case Soldier = 18
-			case Knight = 54
-		}
-	}
+    struct Cost {
+        enum Upgrade: Int {
+            case Village = 8
+            case Unit = 10
+        }
+    }
 }
