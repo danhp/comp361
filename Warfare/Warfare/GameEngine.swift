7dfdd17b642b874dbbc772a4459779a5642f5083
Daniel Pham <pham.dany@gmail.com>
2015-02-11 17:12:41 -0500

    bones of func moveUnit()

--- Warfare/Warfare/GameEngine.swift
+++ Warfare/Warfare/GameEngine.swift
@@ -6,24 +6,24 @@ class GameEngine {
     var currentPlayer = Player()
     var players = [Player]()
     let map = Map()
-    
+
     // MARK: Singleton
-    
+
     class var Instance: GameEngine { return _instance }
-    
+
     // MARK: - Initializers
-    
+
     init() {   }
-    
+
     init(firstPlayer: Int, players: [Player]) {
         assert(players.count == 3, "A Game should be between exactly 3 players.")
-        
+
         self.currentPlayer = players[firstPlayer]
         self.players = players
     }
-    
+
     // MARK: - Serialization
-    
+
     func loadMap(#number: String) {
         var e: NSError?
         if let path = NSBundle.mainBundle().pathForResource(number, ofType: "json") {
@@ -31,16 +31,16 @@ class GameEngine {
                 let data = (json as NSString).dataUsingEncoding(NSUTF8StringEncoding)
                 var parseError: NSError?
                 let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: &parseError)
-                
+
                 if let top = parsedObject as? NSDictionary {
-                    
+
                     // PLAYERS
                     if let players = top["players"] as? NSArray {
                         for p in players {
                             self.players.append(Player(dict: p as NSDictionary))
                         }
                     }
-                    
+
                     // NEUTRAL TILES
                     if let neutral = top["neutral"] as? NSArray {
                         for t in neutral {  // t is an NSDictionary
@@ -52,51 +52,79 @@ class GameEngine {
             }
         }
     }
-    
+
     func serialize() -> NSDictionary {
         var dict = [String: AnyObject]()
-        
+
         dict["players"] = self.players.map({$0.serialize()})
         dict["neutral"] = ""
-        
+
         return dict
     }
-    
+
     func toJSON(dict: NSDictionary) -> String {
         // Make JSON
         var error:NSError?
         var data = NSJSONSerialization.dataWithJSONObject(dict, options:NSJSONWritingOptions(0), error: &error)
-        
+
         // Return as a String
         return NSString(data: data!, encoding: NSUTF8StringEncoding)!
     }
-    
+
     // MARK: - Operations
-    
+
     func beginTurn() {
-        
+
         for village in currentPlayer.villages {
             for tile in village.controlledTiles {
-                
+
                 // Replace tombstones
                 tile.replaceTombstone()
-                
+
                 // Produce constructions
                 if tile.makeRoadOrMeadow() {
                     // Add a new meadow
                     // TODO
                 }
-                
+
                 // Add gold value to village.
                 village.gold += tile.goldValue()
-                
+
                 // Payout wages
                 village.gold += tile.wage()
             }
-            
+
             // Delete the Village
             if village.gold <= 0 {
                 self.currentPlayer.removeVillages(village)
+            }
+        }
+    }
+
+    // Move a unit and update all affected tiles in path.
+    func moveUnit(from: Tile, to: Tile) {
+        if from.unit?.currentAction != Constants.Unit.Action.ReadyForOrders { return }
+
+        for village in currentPlayer.villages {
+            if contains(village.controlledTiles, { $0 === from }) {
+                // TODO: Check if path exists given set of tiles.
+
+                // Update tiles in path
+
+                // Update destination tile.
+                to.removeTombstone()
+
+                if to.chopTree() {
+                    village.wood += 1
+                }
+
+                // TakeOver neutral tile
+
+                // Invade tile
+
+                // Move the unit
+                to.unit = from.unit
+                from.unit = nil
             }
         }
     }
--- Warfare/Warfare/Tile.swift
+++ Warfare/Warfare/Tile.swift
@@ -79,6 +79,21 @@ class Tile: SKShapeNode {
         }
     }
 
+    func removeTombstone() {
+        if self.structure? == Constants.Types.Structure.Tombstone {
+            self.structure = nil
+        }
+    }
+
+    func chopTree() -> Bool {
+        if self.land == .Tree {
+            self.land = .Grass
+            return true
+        }
+
+        return false
+    }
+
     // Builds a road or finishes cultivating a meadow if the required conditions are met
     //
     // @returns True if a meadow was done being cultivated (in which case, according to the requirements, a new meadow should be produce
