002799b57281218693201e96077c885a9e96fc22
HMSimmonds <simmonds.hm@gmail.com>
2015-03-21 15:16:21 -0700

    Added MapselectionViewController

---
+++ Warfare/Warfare/MapSelectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapSelectionViewController.swift
+//  Warfare
+//
+//  Created by Harry Simmonds on 2015-03-21.
+//  Copyright (c) 2015 Justin Domingue. All rights reserved.
+//
+
+import Foundation
