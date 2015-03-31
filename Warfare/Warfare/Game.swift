d4859b6872292b5426189177fa6fbc4ab35b0198
justindomingue <justin.domingue@hotmail.com>
2015-03-31 14:57:46 -0400

    added nameOfActivePlayer property in Game

--- Warfare/Warfare/Game.swift
+++ Warfare/Warfare/Game.swift
@@ -18,6 +18,10 @@ class Game {
 
     var localIsCurrentPlayer: Bool { return GKLocalPlayer.localPlayer().playerID == MatchHelper.sharedInstance().myMatch?.currentParticipant.playerID}
 
+    var nameOfActivePlayer: String {
+        return self.localIsCurrentPlayer ? "Your turn" : ("Player " + String(MatchHelper.sharedInstance().currentParticipantIndex()) + "'s turn")
+    }
+
     var currentPlayerGold: Int { return self.currentPlayer.gold }
     var currentPlayerWood: Int { return self.currentPlayer.wood }
 
